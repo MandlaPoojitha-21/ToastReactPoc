54134701afbe4433693b097592c8ae203033e982
MandlaPoojitha-21 <poojitha.mandla@gmail.com>
2024-09-08 11:46:33 +0530

    setting up main branch as empty

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,6 @@
 import React from 'react'
 import './App.css'
-import Navbar from './Navbar'
-import Herobanner from './Herobanner'
-import Communityactivity from './Communityactivity'
-import Peersonline from './Peersonline'
-import Joingroup from './Joingroup'
-import Discovercommunity from './Discovercommunity'
-import Contributorsandsolutionauthors from './Contributorsandsolutionauthors'
-import Component from './Carousel'
+
 
 function App() {
   
@@ -15,14 +8,7 @@ function App() {
   return (
    <React.Fragment>
    
-    <Navbar/>
-    <Herobanner/>
-    <Communityactivity/>
-    <Peersonline/>
-    <Joingroup/>
-    <Discovercommunity/>
-    <Contributorsandsolutionauthors/>
-
+    <h1>Start putting your components here</h1>
 
     </React.Fragment>
   )
