5074e8545f926e578b293a26ccba6851391119c8
MandlaPoojitha-21 <poojitha.mandla@italentdigital.com>
2024-09-17 23:33:31 +0530

    css fixes

--- src/components/Peersonline/PeersOnline.jsx
+++ src/components/Peersonline/PeersOnline.jsx
@@ -1,17 +1,15 @@
 import './PeersOnline.css'
 import PeersOnlineCarouselBottom from './PeersOnlineCarouselBottom';
 import PeersOnlineCarouselTop from './PeersOnlineCarouselTop';
-// import { totalMembers } from "../../khoros-variables/khoros-variables.js";
+import { customdata } from "../../khoros-variables/khoros-variables.js";
 
 const PeersOnline = () => {
   return (
     <>
       <section className="peersonline">
-        <div className="peersonline-header" >
-          <div className="peersonline-header-top" >
-          </div>
+        <div className="peersonline-header" > 
           <div className="peersonline-header-bottom">
-            <p>Your peers online- what are you waiting for?</p>
+            <p>{customdata.peersonlineheading}</p>
           </div>
         </div>
         <PeersOnlineCarouselTop />
--- src/components/Peersonline/PeersOnlineCarouselBottom.jsx
+++ src/components/Peersonline/PeersOnlineCarouselBottom.jsx
@@ -4,32 +4,8 @@ import "./PeersOnlineCarouselBottom.css";
 import "swiper/css";
 import { Navigation} from "swiper/modules";
 import { ChevronLeft, ChevronRight } from "lucide-react";
-// import carosal_img from '../../assets/carosal-img.png';
 import {customdata} from "../../khoros-variables/khoros-variables.js";
 
-
-
-
-// const carouselItems = [
-//   {
-//     title: "Stay current",
-//     description:
-//       "Be the first to know about new products and features on the Toast product hub.",
-//     image: carosal_img,
-//   },
-//   {
-//     title: "Find your place",
-//     description:
-//       "Looking to connect with other bakery owners? Curious how other pizzerias configured their menu? Join one of our restaurant groups.",
-//       image: carosal_img,
-//   },
-//   {
-//     title: "Earn badges",
-//     description:
-//       "Get recognized for your community contributions. The more you engage, the more you earn.",
-//       image: carosal_img,       
-//   }
-// ];
 
 export default function PeersOnlineCarouselBottom() {
   const prevRef = useRef(null);
