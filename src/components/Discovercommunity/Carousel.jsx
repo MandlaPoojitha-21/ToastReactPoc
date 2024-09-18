a2bcfbf5f39cce49a9d8626b681296bc8ccfe661
MandlaPoojitha-21 <poojitha.mandla@italentdigital.com>
2024-09-18 17:53:40 +0530

    css changes

--- src/components/Discovercommunity/Carousel.jsx
+++ src/components/Discovercommunity/Carousel.jsx
@@ -7,47 +7,6 @@ import { ChevronLeft, ChevronRight } from "lucide-react";
 import {customdata} from "../../khoros-variables/khoros-variables.js";
 
 
-
-
-// const carouselItems = [
-//   {
-//     title: "Stay current",
-//     description:
-//       "Be the first to know about new products and features on the Toast product hub.",
-//     // image: findYourPlace_img,
-//   },
-//   {
-//     title: "Find your place",
-//     description:
-//       "Looking to connect with other bakery owners? Curious how other pizzerias configured their menu? Join one of our restaurant groups.",
-//       // image: Discover_img,
-//   },
-//   {
-//     title: "Earn badges",
-//     description:
-//       "Get recognized for your community contributions. The more you engage, the more you earn.",
-//       // image: Discover03,        
-//   },
-//   {
-//     title: "Connect with Toast",
-//     description:
-//       "Join AMA style events where you can pose questions to Toast leaders and product experts.",
-//       // image: Discover04,
-//   },
-//   {
-//     title: "Share feedback",
-//     description:
-//       "Your opinion matters. Participate in surveys and beta testing to shape the future of Toast products.",
-//       // image: Discover05,
-//   },
-//   {
-//     title: "Learn and grow",
-//     description:
-//       "Access exclusive webinars and tutorials to enhance your skills and grow your business.",
-//       // image: Discover06,
-//   },
-// ];
-
 export default function Carousel() {
   const prevRef = useRef(null);
   const nextRef = useRef(null);
